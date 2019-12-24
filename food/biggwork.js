c179f6f6657bee5940220f712255defc196ccfbb
zhangrong11115 <1848908799@qq.com>
2019-12-24 16:43:19 +0800

    add food/

---
+++ food/biggwork.js
@@ -0,0 +1,26 @@
+window.onload = function(){
+	var imgs = document.getElementsByClassName('big');
+	var im = document.getElementById("sss");
+	function bigger(){
+		im.style.width = '275px';
+  		im.style.height = '294px';
+	}
+	function smaller(){
+		im.style.width = '245px';
+  		im.style.height = '274px';	
+	}
+	/*for (var i = imgs.length - 1; i >= 0; i--) {
+		function bigger(){
+		imgs[i].style.width = '275px';
+  		imgs[i].style.height = '294px';
+	}
+	function smaller(){
+		imgs[i].style.width = '245px';
+  		imgs[i].style.height = '274px';	
+	}*/
+	
+	}
+	
+}
+
+
